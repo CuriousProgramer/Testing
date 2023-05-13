fe1ff4a2eb4f860508aba895f732bbff4c63fa89
Farhan <farhanajmal960d@gmail.com>
2023-05-13 11:27:55 +0500

    Created Next App

---
+++ app/components/layout/Sidebar.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Sidebar() {
+  return (
+    <nav className=''>
+        <div className=''>
+            <h2 className='font-normal text-lg'>Upzaar</h2>
+            <p className='uppercase'>General</p>
+        </div>
+    </nav>
+  )
+}
+
+export default Sidebar
---
+++ postcss.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  theme: {
+    extend: {
+      colors: {
+        primary: '#FF6347',
+        secondary: '#008080',
+      },
+      fontFamily: {
+        sans: ['Open Sans', 'sans-serif'],
+      },
+      spacing: {
+        '72': '18rem',
+        '84': '21rem',
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+}
